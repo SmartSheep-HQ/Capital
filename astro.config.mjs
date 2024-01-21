66c419649badf1b10f2b92b057a1bcf38a28961a
LittleSheep <littlesheep.code@hotmail.com>
2024-01-22 01:06:43 +0800

    :rocket: Launch v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   site: "https://smartsheep.studio",
-  integrations: [mdx(), tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
