f9a4c1291956873e45a2164bf85c494d1567bf4b
LittleSheep <littlesheep.code@hotmail.com>
2024-02-24 22:56:29 +0800

    :chart_with_upwards_trend: Add track code

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { CssBaseline } from "@mui/material";
 import { SITE_DESCRIPTION, SITE_NAME } from "@/app/consts";
 import { theme } from "@/app/theme";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -34,11 +35,19 @@ export default function RootLayout({ children }: Readonly<{
     <body>
     <AppRouterCacheProvider>
       <CssBaseline />
-      <NextTopLoader showAtBottom color="#4a5099" />
+      <SpeedInsights />
+      <NextTopLoader color="#ffffff" />
       <ThemeProvider theme={theme}>
         <AppShell>{children}</AppShell>
       </ThemeProvider>
     </AppRouterCacheProvider>
+
+    <script
+      async
+      src="https://analytics.smartsheep.studio/script.js"
+      data-website-id="bbe87bab-bd5b-416b-8767-b29088c75ab2"
+    />
+
     </body>
     </html>
   );
